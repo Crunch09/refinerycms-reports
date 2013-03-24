73788f3d483b9e79fb104fb5af9a3fd36ceb879b
Florian Thomas <flo@florianthomas.net>
2013-03-24 18:48:52 -0400

    added usable model

---
+++ lib/models/usable.rb
@@ -0,0 +1,12 @@
+class Usable
+
+  attr_accessor :usables
+
+  def self.all
+    return @usables unless @usables.nil?
+    combined = Refinery::Reports::Vehicle.all + Refinery::Reports::Person.all
+    hsh = combined.group_by{|u| u.department.intern? ? 'intern' : 'extern'}
+    @usables = hsh.each{|k,v| hsh[k] = v.group_by{|r| r.department.name}}
+    @usables
+  end
+end
